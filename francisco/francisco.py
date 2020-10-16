b15f5c2cc210b47c534ceea25b80c07d7e7cc3ab
FranciscoN12 <72116239+FranciscoN12@users.noreply.github.com>
2020-10-15 22:55:04 -0500

    python d

---
+++ francisco/francisco.py
@@ -0,0 +1,66 @@
+'''
+
+'''
+#Interger
+
+name = "Francisco"
+print(name)
+
+var1 = 5
+print(var1)
+var2 = 10
+print(var2)
+var3 = ([var2])
+print(var3)
+
+x = 24
+y = 100
+
+#Float
+
+z = 1.1
+print(z)
+a = 2.75
+print(a)
+b = ([a])
+print(b)
+c = 100.1
+print(c)
+d = 12.3
+print(d)
+
+#Strings
+
+name = "Francisco"
+print(name)
+e = "F"
+print(e)
+f = "o"
+print(f)
+g = "Chicken"
+print(g)
+h = ("F","r","a")
+print(h)
+
+#Booleans
+
+done = False
+print(done)
+smart = True
+print(smart)
+i = done
+print(i)
+j = False
+print(j)
+almostDone = True
+print(almostDone)
+
+#Lists
+
+listInt = [5,10,24,100]
+print([5,10,24,100])
+listString = ["f","r","a"]
+print(["f","r","a"])
+listBoolean = [done,smart,i,j,almostDone]
+print(done,smart,i,j,almostDone)
+
