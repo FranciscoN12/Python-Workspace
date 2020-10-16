04e4bb5343d4178c7eac8a2d8e661cd47d1e8f70
FranciscoN12 <72116239+FranciscoN12@users.noreply.github.com>
2020-10-15 22:10:55 -0500

    variable p

---
+++ variables/practice.py
@@ -0,0 +1,60 @@
+'''
+
+[name] = [data]
+
+VarDataVariable
+'''
+
+#Integer
+
+a = 5
+a = 4
+a = 3
+
+#Float
+
+a = 2.54
+
+#String
+
+a="string"
+
+name="Francisco"
+
+#Boolean
+
+a = True
+a = False
+
+#List
+
+a =[2,4,5,6]
+a = ["StringOne"]
+a = [True,False]
+a = [2.54]
+
+varA = 1
+vara = 2
+
+print(varA)
+
+Var = 4
+Var = "string"
+
+
+varTwo = Var
+
+print(varTwo)
+
+b = [a,varA,Var]
+
+List = ["a","b","c"]
+
+print(list[0])
+
+string = "Francisco"
+
+print(string[0])
+print(True)
+
+
