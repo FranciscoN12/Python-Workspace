2dbccce2a3e22a1d058d7557faeed72f79aefe74
FranciscoN12 <72116239+FranciscoN12@users.noreply.github.com>
2020-10-16 20:59:54 -0500

    Python comd

---
+++ comments/commentsDrill.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 16, 2020
+
+@author: ITAUser
+'''
+
+#Hello people
+
+'''
+think smart
+'''
+
+var = 100 #This is a variable
+
+#Nice programming
+
+'''
+practicing
+'''
---
+++ comments/commentsPractice.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 16, 2020
+
+@author: ITAUser
+'''
+
+#vhrwvbi
+
+'''febcahvbe
+cedsbvrjhefsbjhv
+ervaref'''
+
+#This isn't working, can't debug, what do you think?
+
+var = 4 #Single line comment
+print(var)
