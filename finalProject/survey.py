98173fd2856ae3fbab6dfc77c01d9a59dd46d6a7
FranciscoN12 <72116239+FranciscoN12@users.noreply.github.com>
2020-10-30 16:07:17 -0500

    final project 1

---
+++ finalProject/survey.py
@@ -0,0 +1,43 @@
+'''
+Created on Oct 30, 2020
+
+@author: ITAUser
+'''
+'''
+This survey is to get answers from students about what their name is, their
+their favorite color, where they grew up, what is their best friends name, and
+what color hair they have? We will print they answers and store them in 
+variables.
+
+To Do:
+1.create question, What is your name?
+2.create question, What is your favorite color?
+3.create question, What city did you grow up in?
+4.create question, What is your best friends name?
+5.create question, What color hair do you have?
+
+'''
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+name = input("What is your name?")
+print(name)
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+color = input("What is your favorite color?")
+print(color)
+
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+city = input("What city did you grow up in?")
+print(city)
+
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+friend = input("What is your best friends name?")
+print(friend)
+
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+hair = input("What color hair do you have?")
+print(hair)
