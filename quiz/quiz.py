fc8c17e57b6c69733faa45a1d04ee71291a23463
FranciscoN12 <72116239+FranciscoN12@users.noreply.github.com>
2021-02-06 12:23:17 -0600

    conditionalq

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -19,50 +19,59 @@ trivia.
 score = 0
 
 #Ask the user question one. And store the users answer.
-print("What is the powerhouse of the cell?")
+print("What is the power house of the cell?")
+ansOne = input("ENTER A LETTER: A) mitochondria B) nucleous C) ribosome")
 
 #Check if the answer is correct, if it is add one to score and print out
 #"Correct"
 #Else, print out "Incorrect, the correct answer is A."
-a = "mitochondria"
-if(a == "mitochondria"):
-    print("correct")
+if(ansOne.upper() == "A"):
+    score = score + 1
+    print("Correct")
 else:
     print("Incorrect, the correct answer is A")
 
 #Ask the user question two. And store the users answer.
 print("How many states comprise the United States?")
+ansTwo = input("ENTER A LETTER: A) 13 B) 45 C) 50")
 
 #Check if the answer is correct, if it is add one to score and print out
 #"Correct"
 #Else, print out "Incorrect, the correct answer is C."
-c = "50"
-if(c == "50"):
-    print("correct")
+if(ansTwo.upper() == "C"):
+    score = score + 1
+    print("Correct")
 else:
-    print("Incorrect, the correct answer is c")
+    print("Incorrect, the correct answer is C")
+
 #Ask the user question three. And store the users answer.
 print("In y = mx + b, what does m stand for?")
+ansThree = input("A) slope B) output C) I don't understand math")
+
 #Check if the answer is correct, if it is add one to score and print out
 #"Correct"
 #Else, print out "Incorrect, the correct answer is A."
-a = "slope"
-if(a == "slope"):
-    print("correct")
+if(ansThree.upper() == "A"):
+    score = score + 1
+    print("Correct")
 else:
-    print("Incorrect, the correct answer is a")
+    print("Incorrect, the correct answer is A")
+
 #Ask the user question four. And store the users answer.
-print("In English, a person, place or thing is called")
+print("In English, a person, place or thing is called?")
+ansFour = input("A) verb B) adjective C) noun")
+
 #Check if the answer is correct, if it is add one to score and print out
 #"Correct"
 #Else, print out "Incorrect, the correct answer is C."
-c = "noun"
-if(c == "noun"):
+if(ansFour.upper() == "C"):
+    score = score + 1
     print("Correct")
 else:
-    print("Incorrect, the correct answer is c")
-    
+    print("Incorrect, the correct answer is C")
+
 #Calculate the percentage the user got. And store it in a variable called p
 p = score / 4
+
 #Print out the users score: "You got a [score]/4. Or a [p]%."
-print("you got a [score]/4")
+print("You got a " + str(score) + "/4. Or a " + str(p) + "%.")
